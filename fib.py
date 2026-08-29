fcd8d53bf1d2a98a37c751dcb25c73c0c9c87473
Aakesh09 <mallaaakesh@gmail.com>
2026-08-29 15:03:00 +0530

    fibonacci

---
+++ fib.py
@@ -0,0 +1,23 @@
+# n=6
+# def fib(a,b):
+#     if a<=1:
+#         return a
+#     if a in b:
+#         return b[a]
+#     b[a]=fib(a-2,b)+fib(a-1,b)
+#     return b[a]
+# b={}
+# fib(n,b)
+# print(b[n])
+# def fib(n):
+#     dp=[0]*(n+1)
+#     dp[0]=0
+#     if n>=1:
+#         dp[1]=1
+#     for i in range(2,n+1):
+#         dp[i]=dp[i-1]+dp[i-2]
+#     return dp[n]
+# a=11
+# print(fib(a)) 
+
+
